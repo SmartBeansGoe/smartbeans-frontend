94c71a2090d0b70408efd654fafab831d4811a8a
Ole Umlauft <ole.umlauft@protonmail.com>
2020-12-16 12:39:51 +0100

    Added id for the debug examples

--- src/components/character/Body.js
+++ src/components/character/Body.js
@@ -3,15 +3,16 @@ import React, { Component } from 'react'
 export default class Body extends Component {
   render() {
     return (
-      <g>
+      <React.Fragment>
+      <g id="debug_thomas">
         <ellipse
           fill={this.props.color}
-          id="debug_thomas"
           cx="105.63578"
           cy="149.05804"
           rx="52.452663"
           ry="71.488609" />
       </g>
+      </React.Fragment>
     )
   }
 }
--- src/components/character/Face.js
+++ src/components/character/Face.js
@@ -3,11 +3,10 @@ import React, { Component } from 'react'
 export default class Face extends Component {
   render() {
     return (
-      <g>
+      <g id="debug_face">
         <ellipse
           fill="#000000"
           strokeWidth="0.264583"
-          id="path90"
           cx="80.957985"
           cy="94.239716"
           rx="7.9174061"
@@ -15,7 +14,6 @@ export default class Face extends Component {
         <ellipse
           fill="#000000"
           strokeWidth="0.264583"
-          id="path92"
           cx="132.67834"
           cy="93.337196"
           rx="12.247429"
@@ -23,7 +21,6 @@ export default class Face extends Component {
         <ellipse
           fill="#000000"
           strokeWidth="0.264583"
-          id="path94"
           cx="101.6119"
           cy="140.84663"
           rx="40.719944"
