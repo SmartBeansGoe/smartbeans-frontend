95d7af738474125dc4a23c7ca268fe90285853f2
n.rehefeld <n.rehefeld@stud.uni-goettingen.de>
2021-01-09 20:56:00 +0100

    Add basic layout for Submissions Cards

--- src/components/exercises/Submission.js
+++ src/components/exercises/Submission.js
@@ -1,12 +1,45 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 import PropTypes from 'prop-types';
+import { Collapse } from 'react-collapse';
+import { mdiChevronDown, mdiChevronUp } from '@mdi/js';
+import { Icon } from '@mdi/react';
 
 export default class Submission extends Component {
-    componentDidMount() {
+    state = {
+        isOpen: false,
+        duration: "1s"
     }
 
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen,
+        })
+    };
+
     render() {
-        return (<div>Versuch {this.props.id} <p>{this.props.submission.sourceCode}</p> </div>)
+        return (
+            <React.Fragment>
+                <div className="card">
+                    <header className="card-header">
+                        <p className="card-header-title">Versuch {this.props.id}</p>
+                        <a className="card-header-icon" onClick={this.toggle} >
+                            <span className="icon">
+                                <Icon path={this.state.isOpen ? mdiChevronUp : mdiChevronDown} />
+                            </span>
+                        </a>
+                    </header>
+                    <Collapse isOpened={this.state.isOpen}>
+                        <div className="card-content">
+                            <h1 className="title is-6">Quelltext</h1>
+                            <pre><code className="quelltext"></code></pre>
+                            <h1 className="title is-6">Ausgabe des Compliers</h1>
+                            <p></p>
+                            <h1 className="title is-6">Feedback</h1>
+                            <p></p>
+                        </div>
+                    </Collapse>
+                </div>
+            </React.Fragment>)
     }
 }
 
