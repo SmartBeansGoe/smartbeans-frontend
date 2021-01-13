db0538ea1a4d6b0af1f7aa032b8f8d89de1e675c
Ole Umlauft <ole.umlauft@protonmail.com>
2021-01-13 15:55:49 +0100

    Refactored disabling of save button

--- src/components/character/CharacterBuildingPage.js
+++ src/components/character/CharacterBuildingPage.js
@@ -102,21 +102,13 @@ export default class CharacterBuildingPage extends Component {
             <input className="button" type="color" id="body" name="body" value={this.state.body_color} onChange={(event) => this.setBodyColor(event.target.value)}/>
           </span>
           <span>
-            {this.isBodyChanged() ? 
-            <button className="button is-success" onClick={() => 
+            <button className="button is-success" disabled={!this.isBodyChanged()} onClick={() => 
               this.props.onSaveCharacterProperties(
                 this.state.body_color,
                 this.state.shirt_id,
                 this.state.pants_id,
                 this.state.hat_id)
-            }>Save Configurations</button> :
-            <button className="button is-success" disabled onClick={() => 
-              this.props.onSaveCharacterProperties(
-                this.state.body_color,
-                this.state.shirt_id,
-                this.state.pants_id,
-                this.state.hat_id)
-            }>Save Configurations</button>}
+            }>Save Configurations</button>
           </span>
         </div>
         <div className="tabs">
