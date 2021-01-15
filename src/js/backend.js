b42c9a0282992db1605deab861415f22eefcb718
n.rehefeld <n.rehefeld@stud.uni-goettingen.de>
2021-01-16 00:17:37 +0100

    Remove consol log

--- src/js/backend.js
+++ src/js/backend.js
@@ -11,8 +11,6 @@ if (auth_cookie !== undefined)
 else
   auth_token = "not logged in"
 
-console.log(auth_token);
-
 axios.defaults.headers.common['Authorization'] = "Bearer " + auth_token;
 
 const axios_inst = axios.create({
