4eefde2343db42f3c6101a109dea89e3553bfedf
Ole Umlauft <ole.umlauft@protonmail.com>
2021-02-02 12:22:12 +0100

    Added handling of server errors and no connection error.

--- src/errors/Error.js
+++ src/errors/Error.js
@@ -1,15 +1,19 @@
 import lang from '../lang/de_DE.json';
 
 export function handleError(error) {
+  if (error.response === undefined) {
+    handleNoServerConnection();
+    return;
+  }
   let errorCode = error.response.status;
   switch (errorCode) {
     case 400:
       console.log('400 Bad Request');
-      handleNotLoggedIn(error);
+      handleNotLoggedIn();
       break;
     case 401:
       console.log('401 Unauthorized');
-      handleNotLoggedIn(error);
+      handleNotLoggedIn();
       break;
     case 402:
       console.log('402 Payment Required');
@@ -25,18 +29,23 @@ export function handleError(error) {
       break;
     case 500:
       console.log('500 Internal Server Error');
+      handleServerError(500);
       break;
     case 501:
       console.log('501 Not Implemented');
+      handleServerError(501);
       break;
     case 502:
       console.log('502 Bad Gateway');
+      handleServerError(502);
       break;
     case 503:
       console.log('503 Service Unavailable');
+      handleServerError(503);
       break;
     case 504:
       console.log('504 Gateway Timeout');
+      handleServerError(504);
       break;
     default:
       console.log(error.message);
@@ -45,12 +54,20 @@ export function handleError(error) {
 }
 
 function handleNotLoggedIn() {
-  // Shows the error message not-logged-in in the body
+  showErrorMessage(lang['error.not-logged-in']);
+}
+
+function handleNoServerConnection() {
+  showErrorMessage(lang['error.server.no-connection']);
+}
+
+function handleServerError(errorCode) {
+  showErrorMessage(lang['error.server.error'] + ' Error: ' + errorCode);
+}
+
+function showErrorMessage(message) {
   document.getElementById('body').innerHTML =
-    '<div class="notification is-danger is-light has-text-centered">' +
-    lang['error.not-logged-in'] +
+    '<div class="notification is-danger is-light has-text-centered" style="width:100%">' +
+    message +
     '</div>';
-  // Replaces the navigation bar
-  document.getElementById('navbar').innerHTML =
-    '<nav class="navbar is-dark" role="navigation" aria-label="main navigation"> <div class="navbar-brand"><a class="navbar-item"><img src="/favicon.ico" width="30" height="60" alt="" /></a></div></nav>';
 }
