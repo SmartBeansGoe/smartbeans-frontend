c6039b378a895f9d93d1d7bd82785e1540155672
Ole Umlauft <ole.umlauft@protonmail.com>
2021-10-01 23:34:30 +0200

    Add course progress (only tasks as unit is implemented)

--- src/lib/components/dashboard/Level.svelte
+++ src/lib/components/dashboard/Level.svelte
@@ -1,18 +1,48 @@
 <script>
-  export let level = 0;
-  export let maxLevel = 10;
-	export let points = 10;
-	export let maxPoints = 100;
+	import { course, progress, tasks } from '$lib/stores/stores';
+
+	let level;
+	let maxLevel;
+
+	let points = 0;
+	let maxPoints = 0;
+
+	$: if (course.config && $course.config.gamification.level.unit == 'points') {
+		points = calculatePoints($progress, $tasks);
+		level = calculateLevel(points, $course.config.gamification.level.levelSteps);
+		maxLevel =
+			$course.config.gamification.level.levelSteps[
+				$course.config.gamification.level.levelSteps.length
+			];
+	} else {
+		points = $progress.length;
+		maxPoints = $tasks.length;
+	}
+	function calculateLevel(points, levelSteps) {
+		return 0; // TODO
+	}
+
+	function calculatePoints(solvedTasks, tasks) {
+		return 0; // TODO
+	}
 </script>
 
-<div class="text-2xl font-bold">Level {level} von {maxLevel}</div>
-<div class="pt-1">
-	<div class="overflow-hidden h-8 mb-4 flex rounded-xl bg-indigo-200 text-sm font-bold">
-		<div
-			style="width:{(points / maxPoints) * 100}%"
-			class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-indigo-500"
-		>
-			{points}/{maxPoints} Punkte
+{#if $course.config}
+	<div class="text-2xl font-bold">
+		{#if $course.config.gamification.level.unit == 'points'}
+			Level {level}/{$course.config.gamification.level.levelSteps.length}
+		{:else}
+			Fortschritt
+		{/if}
+	</div>
+
+	<div class="pt-1">
+		<div class="relative overflow-hidden h-6 mb-4 flex rounded-md bg-emerald-300 text-sm font-bold">
+			<span style="width:{(points / maxPoints) * 100}%" class="bg-emerald-500" />
+			<span class="absolute flex flex-wrap h-full w-full justify-center text-white content-center"
+				>{points}/{maxPoints}
+				{$course.config ? $course.config.gamification.level.unitName : ''}</span
+			>
 		</div>
 	</div>
-</div>
+{/if}
