ad30aa56f9cb10f2883244ed3980fa8bd9b3f8ef
Ole Umlauft <ole.umlauft@protonmail.com>
2021-10-05 00:50:37 +0200

    Refactoring

--- src/lib/utils/tasks.js
+++ src/lib/utils/tasks.js
@@ -1,8 +1,8 @@
 export function getNextTask(progress, tasks) {
 	let solvedTasks = progress;
-	let unsolvedTask = tasks
+	let nextTask = tasks
 		.filter((task) => !solvedTasks.includes(task.taskid))
 		.sort((a, b) => a.order_by - b.order_by)
 		.find((task) => task.prerequisites.filter((pre) => !solvedTasks.includes(pre)).length == 0);
-	return unsolvedTask;
+	return nextTask;
 }
