08539746591e4958ad6530d28b51ffd7841fb675
Ole Umlauft <ole.umlauft@protonmail.com>
2021-10-06 22:09:04 +0200

    Implements course config

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,13 +3,37 @@
 	import Level from '$lib/components/dashboard/Level.svelte';
 	import Achievements from '$lib/components/dashboard/Achievements.svelte';
 	import TransitionWrapper from '$lib/components/ui/transitions/TransitionWrapper.svelte';
+	import { course, getCourse } from '$lib/stores/stores';
+	import { onMount } from 'svelte';
+	import NextExercise from '$lib/components/dashboard/NextExercise.svelte';
+
+	let levelActive;
+	let achievementsActive;
+	let skillsActive;
+
+	$: if ($course.config) {
+		levelActive = $course.config.gamification.level.onDashboard;
+		achievementsActive = $course.config.gamification.achievements.onDashboard;
+		skillsActive = $course.config.gamification.skillgraph.onDashboard;
+	}
+
+	onMount(async () => {
+		if ($course.length == 0) getCourse();
+	});
 </script>
 
 <TransitionWrapper>
 	<div class="p-4 grid grid-cols-1 md:grid-cols-2 gap-4 h-full">
-		<div class="box"><Level /></div>
-		<div class="box"><Achievements /></div>
-		<div class="box"><SkillGraph /></div>
+		{#if levelActive}
+			<div class="box"><Level /></div>
+		{/if}
+		{#if achievementsActive}
+			<div class="box"><Achievements /></div>
+		{/if}
+		<div class="box"><NextExercise /></div>
+		{#if skillsActive}
+			<div class="box"><SkillGraph /></div>
+		{/if}
 	</div>
 </TransitionWrapper>
 
