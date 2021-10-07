075586417898ed43a713eef3b6cf65a764098859
Ole Umlauft <ole.umlauft@protonmail.com>
2021-10-07 23:56:53 +0200

    Add pad_url const to config sample

--- src/lib/config/config.sample.js
+++ src/lib/config/config.sample.js
@@ -3,3 +3,4 @@ export const sessionDuration = 3600; // Must fit the time in the Backend configu
 export const staticAssetPath = "/img/assets"
 export const staticAssetFilePath = '/img/assets/assets.json';
 export const frontend_url = 'change-this';
+export const pad_url = 'change-this';
