4b69d68baf1324754ec9189d2a8c2958b321c791
Ole Umlauft <ole.umlauft@protonmail.com>
2021-10-12 01:02:59 +0200

    Add graph link store to save the position of links

--- src/lib/components/tasks/complex/Links.svelte
+++ src/lib/components/tasks/complex/Links.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { graphLinksStore } from '$lib/stores/graph';
 	import { onMount } from 'svelte';
 
 	export let links;
@@ -14,56 +15,63 @@
 	});
 
 	function setDimensions(node, link) {
-		let from = document.getElementById('taskid:' + link.source);
-		let to = document.getElementById('taskid:' + link.target);
+		if ($graphLinksStore[link.source + '-' + link.target] == undefined) {
+			let from = document.getElementById('taskid:' + link.source);
+			let to = document.getElementById('taskid:' + link.target);
 
-		let svgTop = from.offsetTop > to.offsetTop ? to.offsetTop : from.offsetTop;
-		let svgBottom =
-			from.offsetTop + from.offsetHeight > to.offsetTop + to.offsetHeight
-				? from.offsetTop + from.offsetHeight
-				: to.offsetTop + to.offsetHeight;
+			let svgTop = from.offsetTop > to.offsetTop ? to.offsetTop : from.offsetTop;
+			let svgBottom =
+				from.offsetTop + from.offsetHeight > to.offsetTop + to.offsetHeight
+					? from.offsetTop + from.offsetHeight
+					: to.offsetTop + to.offsetHeight;
 
-		let svgWidth = to.offsetLeft - (from.offsetLeft + from.offsetWidth);
-		let svgHeight = svgBottom - svgTop;
+			let svgWidth = to.offsetLeft - (from.offsetLeft + from.offsetWidth);
+			let svgHeight = svgBottom - svgTop;
 
-		node.style.top = (from.offsetTop > to.offsetTop ? to.offsetTop : from.offsetTop) + 'px';
-		node.style.left = from.offsetLeft + from.offsetWidth + 'px';
+			let posStart = { x: 0, y: from.offsetTop + from.offsetHeight / 2 - svgTop };
+			let posEnd = { x: svgWidth, y: to.offsetTop + to.offsetHeight / 2 - svgTop };
+
+			$graphLinksStore[link.source + '-' + link.target] = {};
+			$graphLinksStore[link.source + '-' + link.target]['svgTop'] = svgTop;
+			$graphLinksStore[link.source + '-' + link.target]['svgBottom'] = svgBottom;
+			$graphLinksStore[link.source + '-' + link.target]['svgWidth'] = svgWidth;
+			$graphLinksStore[link.source + '-' + link.target]['svgHeight'] = svgHeight;
+			$graphLinksStore[link.source + '-' + link.target]['fromOffsetTop'] = from.offsetTop;
+			$graphLinksStore[link.source + '-' + link.target]['toOffsetTop'] = to.offsetTop;
+			$graphLinksStore[link.source + '-' + link.target]['fromOffsetLeft'] = from.offsetLeft;
+			$graphLinksStore[link.source + '-' + link.target]['fromOffsetWidth'] = from.offsetWidth;
+			$graphLinksStore[link.source + '-' + link.target]['posStart'] = posStart;
+			$graphLinksStore[link.source + '-' + link.target]['posEnd'] = posEnd;
+		}
+
+		let svgWidth = $graphLinksStore[link.source + '-' + link.target].svgWidth;
+		let svgHeight = $graphLinksStore[link.source + '-' + link.target].svgHeight;
+		let fromOffsetTop = $graphLinksStore[link.source + '-' + link.target]['fromOffsetTop'];
+		let toOffsetTop = $graphLinksStore[link.source + '-' + link.target]['toOffsetTop'];
+		let fromOffsetLeft = $graphLinksStore[link.source + '-' + link.target]['fromOffsetLeft'];
+		let fromOffsetWidth = $graphLinksStore[link.source + '-' + link.target]['fromOffsetWidth'];
+
+		node.style.top = (fromOffsetTop > toOffsetTop ? toOffsetTop : fromOffsetTop) + 'px';
+		node.style.left = fromOffsetLeft + fromOffsetWidth + 'px';
 		node.style.height = svgHeight + 'px';
 		node.style.width = svgWidth + 'px';
 	}
 
 	function drawLink(node, link) {
-		let from = document.getElementById('taskid:' + link.source);
-		let to = document.getElementById('taskid:' + link.target);
-
-		let svgWidth = to.offsetLeft - (from.offsetLeft + from.offsetWidth);
-		let svgTop = from.offsetTop > to.offsetTop ? to.offsetTop : from.offsetTop;
-
-		let posStart = { x: 0, y: from.offsetTop + from.offsetHeight / 2 - svgTop };
-		let posEnd = { x: svgWidth, y: to.offsetTop + to.offsetHeight / 2 - svgTop };
+		let posStart = $graphLinksStore[link.source + '-' + link.target]['posStart'];
+		let posEnd = $graphLinksStore[link.source + '-' + link.target]['posEnd'];
 
 		let color = progress.includes(link.source) ? '#6EE7B7' : '#FCA5A5';
 		node.setAttribute('stroke', color);
 
-		if (svgWidth <= 64) {
-			node.setAttribute(
-				'd',
-				`M ${posStart.x},${posStart.y} C ${posStart.x},${posStart.y} ${posStart.x + posEnd.x / 2},${
-					posStart.y
-				} ${posStart.x + posEnd.x / 2},${(posStart.y + posEnd.y) / 2} ${
-					posStart.x + posEnd.x / 2
-				},${posEnd.y} ${posEnd.x},${posEnd.y} ${posEnd.x},${posEnd.y}`
-			);
-		} else {
-			node.setAttribute(
-				'd',
-				`M ${posStart.x},${posStart.y} C ${posStart.x},${posStart.y} ${posStart.x + posEnd.x / 2},${
-					posStart.y
-				} ${posStart.x + posEnd.x / 2},${(posStart.y + posEnd.y) / 2} ${
-					posStart.x + posEnd.x / 2
-				},${posEnd.y} ${posEnd.x},${posEnd.y} ${posEnd.x},${posEnd.y}`
-			);
-		}
+		node.setAttribute(
+			'd',
+			`M ${posStart.x},${posStart.y} C ${posStart.x},${posStart.y} ${posStart.x + posEnd.x / 2},${
+				posStart.y
+			} ${posStart.x + posEnd.x / 2},${(posStart.y + posEnd.y) / 2} ${posStart.x + posEnd.x / 2},${
+				posEnd.y
+			} ${posEnd.x},${posEnd.y} ${posEnd.x},${posEnd.y}`
+		);
 	}
 </script>
 
