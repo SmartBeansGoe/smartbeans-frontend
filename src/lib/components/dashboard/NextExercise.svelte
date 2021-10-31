8a59937334f701efa2bac77fa5c50d3262dab1b4
Ole Umlauft <ole.umlauft@protonmail.com>
2021-10-31 22:58:59 +0100

    Change NextExercise component on dashboard

--- src/lib/components/dashboard/NextExercise.svelte
+++ src/lib/components/dashboard/NextExercise.svelte
@@ -3,7 +3,7 @@
 
 	import { progress, tasks } from '$lib/stores/stores';
 	import { getNextTask } from '$lib/utils/tasks';
-	import marked from 'marked';
+	import Tag from '../ui/Tag.svelte';
 
 	$: task = getNextTask($progress, $tasks);
 </script>
@@ -13,23 +13,25 @@
 		class="h-full cursor-pointer overflow-y-hidden"
 		on:click={() => goto(`/tasks/${task.taskid}`)}
 	>
-		<div class=" m-4 ">
-			<p class="text-2xl"><b>Nächste Aufgabe:</b> <i>{task.task_description.shortname}</i></p>
-			<div>
-				{#each task.tags as tag}
-					<div
-						class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-indigo-600 bg-indigo-200 hover:bg-indigo-300 hover:text-indigo-600 uppercase last:mr-0 mr-1 hover:cursor-pointer"
-						on:click={(event) => {
-							event.stopPropagation();
-							goto(`/tasks?category=${tag.name}`);
-						}}
-					>
-						{tag.name}
-					</div>
-				{/each}
-			</div>
-			<div class="">
-				<p class="italic">{@html marked(task.task_description.task)}</p>
+		<div class="m-4">
+			<p class="text-2xl">
+				<b>Nächste Aufgabe:</b> <i>{task.task_description.title}</i>
+			</p>
+			<div class="pt-3">
+				<span class="pr-4">{task.task_description.shortname}:</span>
+				<span>
+					{#each task.tags as tag}
+						<Tag
+							color="link"
+							on:click={(event) => {
+								event.stopPropagation();
+								goto(`/tasks?category=${tag.name}`);
+							}}
+						>
+							{tag.name}
+						</Tag>
+					{/each}
+				</span>
 			</div>
 		</div>
 	</div>
